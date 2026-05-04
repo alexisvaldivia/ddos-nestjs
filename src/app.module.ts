78105b27ad0eadd6e48c213854e2af978e9513b6
alexisvaldivia <alesixvaldivia@gmail.com>
2026-05-04 12:41:39 -0300

    Instalé y configuré el ThrottlerModule globalmente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { LoginModule } from './login/login.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [LoginModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 10000,
+          limit: 3,
+        },
+      ],
+    }),
+    LoginModule,
+  ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
